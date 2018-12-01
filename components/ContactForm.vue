89bdd57303e38e7bf28de1d325aec27a52682f0c
Jake <jakenielsonweb@gmail.com>
2018-11-30 22:38:34 -0700

    contact form skeleton

---
+++ components/ContactForm.vue
@@ -0,0 +1,51 @@
+<template>
+  <form class="contact-form">
+    <div class="contact-form__section">
+      <label class="contact-form__label" for="email">Email</label>
+      <input class="contact-form__text" v-model="email" type="text" id="email" name="email">
+    </div>
+    <div class="contact-form__section">
+      <label class="contact-form__label" for="message">Message</label>
+      <textarea class="contact-form__textarea" v-model="message" id="message" name="message" rows="10"></textarea>
+    </div>
+    <button class="contact-form__button" @click.prevent="submit">Submit</button>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: '',
+      message: '',
+    }
+  },
+  methods: {
+    submit() {
+      console.log('email', this.email);
+      console.log('message', this.message);
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.contact-form__section {
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 20px;
+}
+.contact-form__label {
+  font-size: 32px;
+  margin-bottom: 5px;
+}
+.contact-form__text {
+  width: 250px;
+}
+.contact-form__text, .contact-form__textarea {
+  font-size: 18px;
+}
+.contact-form__button {
+  font-size: 18px;
+}
+</style>
---
+++ pages/contact.vue
@@ -0,0 +1,69 @@
+<template>
+  <section class="util__container">
+    <component v-if="story.content.component" :key="story.content._uid" :blok="story.content" :is="story.content.component"></component>
+    <contact-form></contact-form>
+  </section>
+</template>
+
+<script>
+import storyblokLivePreview from '@/mixins/storyblokLivePreview';
+import ContactForm from '~/components/ContactForm.vue';
+
+export default {
+  components: {
+    ContactForm
+  },
+  data () {
+    return { story: { content: {} } }
+  },
+  computed: {
+    ogImage () {
+      return `https://${this.story.content.og_image.slice(2)}`;
+    },
+    twitterImage () {
+      return `https://${this.story.content.twitter_image.slice(2)}`;
+    },
+  },
+  mixins: [storyblokLivePreview],
+  asyncData (context) {
+    // Check if we are in the editor mode
+    let version = context.query._storyblok || context.isDev ? 'draft' : 'published'
+
+    // Load the JSON from the API
+    return context.app.$storyapi.get('cdn/stories/contact', {
+      version: version
+    }).then((res) => {
+      return res.data
+    }).catch((res) => {
+      context.error({ statusCode: res.response.status, message: res.response.data })
+    })
+  },
+  head() {
+    return {
+      title: 'Contact',
+      titleTemplate: 'croak.io | %s',
+      meta: [
+        {'charset': 'utf-8'},
+        {'Content-Type': 'text/html'},
+        {'name': 'viewport', 'content': 'width=device-width, initial-scale=1'},
+        {'vmid': 'description', 'name': 'description', 'content': this.story.content.description},
+        {'vmid': 'og:title', 'property': 'og:title', 'content': this.story.content.title},
+        {'vmid': 'og:site_name', 'property': 'og:site_name', 'content': 'croak.io'},
+        {'vmid': 'og:type', 'property': 'og:type', 'content': 'website'},
+        {'vmid': 'og:url', 'property': 'og:url', 'content': 'https://croak.io'},
+        {'vmid': 'og:image', 'property': 'og:image', 'content': this.ogImage},
+        {'vmid': 'og:description', 'property': 'og:description', 'content': this.story.content.description},
+        {'vmid': 'twitter:card', 'name': 'twitter:card', 'content': 'summary'},
+        {'vmid': 'twitter:site', 'name': 'twitter:site', 'content': '@croak_io'},
+        {'vmid': 'twitter:title', 'name': 'twitter:title', 'content': this.story.content.title},
+        {'vmid': 'twitter:description', 'name': 'twitter:description', 'content': this.story.content.description},
+        {'vmid': 'twitter:image', 'name': 'twitter:image', 'content': this.twitterImage},
+        {'vmid': 'twitter:image:alt', 'name': 'twitter:image:alt', 'content': this.story.content.image_alt},
+        {'vmid': 'itemprop:name', 'itemprop': 'name', 'content': this.story.content.title},
+        {'vmid': 'itemprop:description', 'itemprop': 'description', 'content': this.story.content.description},
+        {'vmid': 'itemprop:image', 'itemprop': 'image', 'content': this.ogImage},
+      ]
+    }
+  }
+}
+</script>
